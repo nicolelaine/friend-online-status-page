eeb6713a1f4a0faf030519ed3686b4785718b3cf
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-01-27 11:53:47 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+import { useState, useEffect } from "react";
+import Friend from "./Friend";
+
+const App = () => {
+  const [friendData, setFriendData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const request = await fetch(
+          "https://gist.githubusercontent.com/skillcrush-curriculum/8231fb5350d726663f2bb16af7cf6945/raw/0dde3b3942214ed39a08479b334c752bea05b3a6/friend-status.json "
+        );
+        const friendStatus = await request.json();
+        setFriendData(friendStatus);
+      } catch (e) {
+        console.log("Something went wrong", e);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      {friendData.map((friend) => {
+        return <Friend key={friend.id} friend={friend} />;
+      })}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Friend.js
@@ -0,0 +1,16 @@
+const Friend = ({ friend }) => {
+  const letterAvatar = friend.name.split("")[0].toUpperCase();
+  return (
+    <div className="friend-card">
+      <div className="friend-details">
+        <div className="avatar">{letterAvatar}</div>
+        <h2 className="friend-name">{friend.name}</h2>
+      </div>
+      <div
+        className={`friend-status ${friend.online ? "online" : "offline"}`}
+      ></div>
+    </div>
+  );
+};
+
+export default Friend;
